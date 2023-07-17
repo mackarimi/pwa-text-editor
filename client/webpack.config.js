3ea3bb99f9e238653cfe16284c9123f12575d3f2
mackarimi <kkarimi47@gmail.com>
2023-07-17 14:26:04 -0700

    webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -5,34 +5,64 @@ const { InjectManifest } = require("workbox-webpack-plugin");
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
+
 module.exports = () => {
   return {
-    mode: 'development',
+    mode: "development",
     entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
+      main: "./src/js/index.js",
+      install: "./src/js/install.js",
     },
     output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist"),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: "./src/index.html",
+        chunks: ["main"],
+      }),
+      new HtmlWebpackPlugin({
+        filename: "install.html",
+        template: "./src/install.html",
+        chunks: ["install"],
+      }),
+      new WebpackPwaManifest({
+        name: "Your Text Editor",
+        short_name: "Text Editor",
+        description: "A text editor PWA",
+        background_color: "#ffffff",
+        theme_color: "#000000",
+        icons: [
+          {
+            src: path.resolve("./src/images/logo.png"),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join("assets", "icons"),
+            purpose: "any maskable",
+          },
+        ],
+      }),
+      new InjectManifest({
+        swSrc: "./src-sw.js",
+        swDest: "sw.js",
+      }),
     ],
-
     module: {
       rules: [
         {
           test: /\.css$/,
-          use: ['style-loader', 'css-loader'],
+          use: ["style-loader", "css-loader"],
         },
         {
           test: /\.js$/,
           exclude: /node_modules/,
           use: {
-            loader: 'babel-loader',
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+            },
           },
-        }  
+        },
       ],
     },
   };
